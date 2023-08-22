250a336043bbfeff2679dc47bf5bc45957a497fb
Colin Spangelo <colin.spangelo@gmail.com>
2023-08-21 18:13:36 -0600

    Added favorite button clickibility to photo items.

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,22 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+    //Sets the state of the click
+    const [click, setClick] = useState(false);
+
+    const handleClick=function(){
+    
+      //This will change the previous value of state and set it to the opposite value
+      setClick((prev)=>{
+        return !prev
+      })
+     
+    }
+    
   return (
-    <div className="photo-list__fav-icon">
+    <div onClick={handleClick} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={click} />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   console.log(props)
   return(
 <div className="photo-list__item">
+  <PhotoFavButton/>
   <img className="photo-list__image" src={props.imgURL}/>
   <div className="photo-list__user-details">
   <img className="photo-list__user-profile" src={props.userImgURL}/>
